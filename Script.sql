8bd3058c2084f15fdfe2e10383797b95dedf366e
unknown <kkolyagin@mail.ru>
2022-11-07 01:23:38 +0300

    add 4e

--- Script.sql
+++ Script.sql
@@ -11,3 +11,11 @@ select  titlelevel, sum((date_part('year', current_date)- date_part('year',first
 select c.fio, d.title as department from customers c left join departments d on c.departmentid = d.id order by d.id;
 
 --e.     Выведите название отдела и фамилию сотрудника с самой высокой зарплатой в данном отделе и саму зарплату также.
+--select  c.departmentid, max(c.salary)  from customers c group by c.departmentid
+
+select y.department, c1.fio, c1.salary as maxsalary from customers c1 
+right join 
+(select d.title as department, x.maxsalary from departments d 
+right join (select  c.departmentid, max(c.salary) as maxsalary  from customers c group by c.departmentid) x
+on d.id = x.departmentid) y
+on c1.salary = y.maxsalary; 
